22e40d82a290bc094fd56bc46d9f5880c710b39b
codevski <1435321+codevski@users.noreply.github.com>
2026-04-17 05:11:19 +1000

    Update fladder.rb

--- Casks/fladder/fladder.rb
+++ Casks/fladder/fladder.rb
@@ -1,6 +1,6 @@
 cask "fladder" do
   version "0.10.3"
-  sha256 "sha256:567cf48ed983a6d72b7a113b0eb8324f3149268a62aec2da3be96ed55fd777ea"
+  sha256 "567cf48ed983a6d72b7a113b0eb8324f3149268a62aec2da3be96ed55fd777ea"
 
   url "https://github.com/DonutWare/Fladder/releases/download/v#{version}/Fladder-macOS-#{version}.dmg"
   name "fladder"
