14a62fddbf6aa332bb869af71ab1268a930aae9a
codevski <1435321+codevski@users.noreply.github.com>
2026-06-09 10:04:55 +1000

    Bump Feishin to 1.13.0 and add checksums
    Set macOS dependency to :catalina (was '>= :catalina')

--- Casks/feishin/feishin.rb
+++ Casks/feishin/feishin.rb
@@ -1,10 +1,10 @@
 cask "feishin" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.12.1"
+  version "1.13.0"
 
-  sha256 :no_check
-
+  sha256 arm:   "967d6476ae3a06549798f466a64aeeefc39408dcfe271cb753b4c67d8b384b69",
+         intel: "da501d8fe9b0150505f4a6b91b4af4aa2bc4cd619339c4c814d1df7c460244a6"
   url "https://github.com/jeffvli/feishin/releases/download/v#{version}/Feishin-#{version}-mac-#{arch}.dmg",
       verified: "github.com/jeffvli/feishin/"
   name "Feishin"
@@ -16,7 +16,7 @@ cask "feishin" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: :catalina
 
   app "Feishin.app"
 
